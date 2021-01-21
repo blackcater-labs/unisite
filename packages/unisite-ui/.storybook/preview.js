e161ef40c8e9718feaac260a9fbd06fc3b58799e
Elon Tang <blackcater2015@gmail.com>
2021-01-21 17:06:11 +0800

    feat(@unisite/ui): downgrade to postcss@7

--- packages/unisite-ui/.storybook/preview.js
+++ packages/unisite-ui/.storybook/preview.js
@@ -1,3 +1,10 @@
+import React from "react";
+import { addDecorator } from "@storybook/react";
+
+import Layout from "./components/Layout";
+
+addDecorator((storyFn) => <Layout>{storyFn()}</Layout>);
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 };
